f90bbe37bf1ce5c73b8d8db40d5efde7b175d7ec
Bikaze <clmntmugisha@gmail.com>
2024-07-03 10:43:41 +0200

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for calculating the floor of a float.
+
+This module provides a function that returns the floor of a given float number.
+"""
+
+
+def floor(n: float) -> int:
+    """Calculate the floor of a float number.
+
+    Args:
+        n (float): The float number to calculate the floor of.
+
+    Returns:
+        int: The floor of `n`.
+    """
+    return int(n)
