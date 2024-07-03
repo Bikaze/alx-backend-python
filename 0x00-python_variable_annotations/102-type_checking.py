3e33a4d07ccca01930cd914113af4911f5fc510b
Bikaze <clmntmugisha@gmail.com>
2024-07-04 01:24:20 +0200

    Fixed pycodestyle errors

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,9 +7,8 @@ elements in a tuple by a specified factor.
 from typing import List, Tuple, Union
 
 
-from typing import List, Tuple, Union
-
-def zoom_array(lst: Union[Tuple[int, ...], List[int]], factor: int = 2) -> List[int]:
+def zoom_array(lst: Union[Tuple[int, ...], List[int]],
+               factor: int = 2) -> List[int]:
     """
     Replicates each element in a tuple or list by a specified factor.
 
@@ -18,7 +17,8 @@ def zoom_array(lst: Union[Tuple[int, ...], List[int]], factor: int = 2) -> List[
         factor: The replication factor for each element in `lst`.
 
     Returns:
-        A list of integers with each element from the input list or tuple replicated `factor` times.
+        A list of integers with each element from the input list or
+        tuple replicated `factor` times.
     """
     zoomed_in: List[int] = [
         item for item in lst
