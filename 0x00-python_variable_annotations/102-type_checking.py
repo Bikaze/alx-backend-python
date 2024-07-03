861584e52742e2427ea8b2870c318aa3732059e0
Bikaze <clmntmugisha@gmail.com>
2024-07-04 01:27:56 +0200

    Update 102-task

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,21 +4,20 @@ This module provides a function `zoom_array` that replicates
 elements in a tuple by a specified factor.
 """
 
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
 
-def zoom_array(lst: Union[Tuple[int, ...], List[int]],
-               factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     """
-    Replicates each element in a tuple or list by a specified factor.
+    Replicates each element in a tuple by a specified factor.
 
     Args:
-        lst: A list or tuple of integers to be zoomed.
+        lst: A tuple of integers to be zoomed.
         factor: The replication factor for each element in `lst`.
 
     Returns:
-        A list of integers with each element from the input list or
-        tuple replicated `factor` times.
+        A list of integers with each element from the input tuple
+        replicated `factor` times.
     """
     zoomed_in: List[int] = [
         item for item in lst
