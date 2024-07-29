6c4f7de12ba67f37abea3852ab235afea08d694d
Bikaze <clmntmugisha@gmail.com>
2024-07-29 15:49:04 +0200

    Update test_utils.py with map_exception..

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -32,6 +32,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict[str, Any],
+                                         path: Tuple[str, ...]) -> None:
+        """
+        Test access_nested_map raises KeyError for invalid paths.
+
+        Args:
+            nested_map (Dict[str, Any]): The nested map to access.
+            path (Tuple[str, ...]): The path to access in the nested map.
+        """
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+
 
 if __name__ == "__main__":
     unittest.main()
